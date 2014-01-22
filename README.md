e4f22ba960b8e9f092a6249b3e3e3aee9c2483a6
Yak! <yak_ex@mx.scn.tv>
2014-01-22 13:07:01 +0900

    Add a vanilla script.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::ConfigMaker - A tiny helper for configuration files on different environments
+
+# VERSION
+
+version v0.0.0
+
+# SYNOPSIS
+
+# DESCRIPTION
+
+# AUTHOR
+
+Yasutaka ATARASHI <yakex@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Yasutaka ATARASHI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/App/ConfigMaker.pm
+++ lib/App/ConfigMaker.pm
@@ -3,8 +3,28 @@ package App::ConfigMaker;
 use strict;
 use warnings;
 
-# ABSTRACT:
+# ABSTRACT: A tiny helper for configuration files on different environments
 # VERSION
+
+use Getopt::Std;
+use Getopt::Config::FromPod;
+use Pod::Usage;
+use YAML::Any;
+
+my $yaml = YAML::Any->implementation;
+my $encoder = $yaml eq 'YAML::Syck' || $yaml eq 'YAML::Old' ? sub { shift; } : sub { Encode::encode('utf-8', shift); };
+
+sub run
+{
+	shift if @_ && eval { $_[0]->isa(__PACKAGE__) };
+
+	local (@ARGV) = @_;
+
+	my %opts;
+	getopts(Getopt::Config::FromPod->string, \%opts);
+	pod2usage(-verbose => 2) if exists $opts{h};
+	pod2usage(-msg => 'At least one argument MUST be specified', -verbose => 0, -exitval => 1) if ! @ARGV;
+}
 
 1;
 __END__
